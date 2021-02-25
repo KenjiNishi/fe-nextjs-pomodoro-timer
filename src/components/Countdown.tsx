59b5f51fa17891c47e8651bfb89a1ea30b82cf6c
KenjiNishi <wk.nishizawa@gmail.com>
2021-02-25 19:03:10 -0300

    Included hook in Countdown

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,17 +1,50 @@
 import styles from '../styles/components/Countdown.module.css';
 
+import {useState, useEffect} from 'react';
+
 export function Countdown(){
+    const [time,setTime] = useState(0.5 * 60);
+    const [active, setActive] = useState(false);
+
+    const minutes = Math.floor(time/60);
+    const seconds = Math.floor(time%60);
+    
+    const [minuteL, minuteR] = String(minutes).padStart(2,'0').split('');
+    const [secondL, secondR] = String(seconds).padStart(2,'0').split('');
+
+    function startCountdown(){
+        setActive(true);
+
+    }
+
+    useEffect(()=>{
+        if (active && time > 0){
+            setTimeout(()=>{
+                setTime(time-1);
+            }, 1000)
+        }
+        if (time === 0){
+            setActive(false);
+        }
+    }, [active, time])
+
     return(
-        <div className={styles.countdownContainer}>
-            <div>
-                <span>2</span>
-                <span>5</span>
+        <div>
+            <div className={styles.countdownContainer}>
+                <div>
+                    <span>{minuteL}</span>
+                    <span>{minuteR}</span>
+                </div>
+                <span>:</span>
+                <div>
+                    <span>{secondL}</span>
+                    <span>{secondR}</span>
+                </div>
             </div>
-            <span>:</span>
-            <div>
-                <span>3</span>
-                <span>0</span>
-            </div>
+
+            <button type="button" className={styles.countdownButton} onClick={startCountdown}>
+                Begin cycle!
+            </button>
         </div>
         
     )
