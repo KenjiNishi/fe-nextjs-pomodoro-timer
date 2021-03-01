f907dd149b91435a080762611ff0c5457d07ec66
KenjiNishi <wk.nishizawa@gmail.com>
2021-03-01 16:06:13 -0300

    context on completedChallenges component

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,10 +1,14 @@
 import styles from '../styles/components/CompletedChallenges.module.css'
+import {useContext} from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 
 export function CompletedChallenges(){
+    const {challengesCompleted} = useContext(ChallengesContext);
+
     return(
         <div className={styles.completedChallengesContainer}>
             <span>Challenges completed: </span>
-            <span>YYY</span>
+            <span>{challengesCompleted}</span>
         </div>
     );
 }
