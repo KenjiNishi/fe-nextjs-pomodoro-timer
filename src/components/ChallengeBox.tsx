6cf33374e3f2336bd15db2afae83be4774108cbc
KenjiNishi <wk.nishizawa@gmail.com>
2021-03-01 15:00:35 -0300

    Challengebox styling

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,15 +1,41 @@
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox(){
+    const hasActiveChallenge = true;
     return(
         <div className={styles.challengeBoxContainer}>
-            <div className={styles.challengeNotActive}>
-                <strong>Finish a cycle to receive challenges.</strong>
-                <p>
-                    <img src='icons/level-up.svg' alt='level-up'/>
-                    To level up, complete the challenges!
-                </p>
-            </div>
+            {hasActiveChallenge? (
+                <div className={styles.challengeActive}>
+                    <header>Reward: ZZZ xp</header>
+                    <main>
+                        <img src="icons/body.svg"/>
+                        <strong>CHALLENGE_NAME</strong>
+                        <p>CHALLENGE_DESCRIPTION</p>
+                    </main>
+                    <footer>
+                        <button 
+                            type="button"
+                            className={styles.challengeFailedButton}
+                        >
+                            Failed
+                        </button>
+                        <button
+                            type="button"
+                            className={styles.challengeSuccededButton}
+                        >
+                            Completed
+                        </button>
+                    </footer>
+                </div>
+            ):(
+                <div className={styles.challengeNotActive}>
+                    <strong>Finish a cycle to receive challenges.</strong>
+                    <p>
+                        <img src='icons/level-up.svg' alt='level-up'/>
+                        To level up, complete the challenges!
+                    </p>
+                </div>
+            )}
         </div>
     )
 }
